270b9a154b3467ec1c1c50890fd3a313e46b6259
WaffleWaldo <oswaldoortiz0519@gmail.com>
2020-10-28 10:32:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(nameArray){
+    let newArray = [];
+    for(let i = 0; i < nameArray.length; i++){
+        newArray.push(`Thank you, ${nameArray[i]}, for the wonderful surprise gift!`)
+    }
+    return newArray
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num--);
+    }
+}
